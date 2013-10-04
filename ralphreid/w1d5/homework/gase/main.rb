d8e5e26a0c6db7a0d7b79bd7c590bfbbf45d7fb8
Ralph Reid <beresfordjunior@me.com>
2013-10-04 16:46:21 +0100

    End of day materials

---
+++ ralphreid/w1d5/homework/gase/main.rb
@@ -0,0 +1,15 @@
+# the file we use to launch the appliction. this file should contain the logic of the menu
+# and call the function depending on user choice
+
+# there should be seven options in the menu when launched; when you chose it
+# should run a function in main.rb
+
+
+
+brokerage = Brokerage.new("Goldman Assembly")
+
+# should call function to fake data here
+
+# stuff showing menu
+
+
---
+++ ralphreid/w1d5/homework/gase/portfolio.rb
@@ -0,0 +1,15 @@
+class Portfolio
+  attr_accessor :name, :stocks #if you have AAPL you could have mulitpls shares
+
+  def buy_stock
+  end
+
+  def sell_stock
+
+  end
+
+  def position
+
+  end
+
+end
