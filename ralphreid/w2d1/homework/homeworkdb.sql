bbec95822cd73b49bd75a93736a7e74b83e90665
Ralph Reid <beresfordjunior@me.com>
2013-10-07 23:17:56 +0100

    release of w2d1 homework

---
+++ ralphreid/w2d1/homework/homeworkdb.sql
@@ -0,0 +1,20 @@
+
+CREATE TABLE poducts
+
+-- Products
+-- id
+-- created_at
+-- name
+-- on_sale (Boolean)
+-- Price
+
+-- Users
+-- id
+-- created_at
+-- name
+
+-- wishlists
+-- id
+-- created_at
+-- user_id
+-- product_id
