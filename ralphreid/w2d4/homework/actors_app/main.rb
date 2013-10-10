e184a8274b34a5d9ae99e5326e2abcf474c17d27
Ralph Reid <beresfordjunior@me.com>
2013-10-10 19:09:49 +0100

    release of w2d4-layout with Add an actor - feature

--- ralphreid/w2d4/homework/actors_app/main.rb
+++ ralphreid/w2d4/homework/actors_app/main.rb
@@ -14,36 +14,36 @@ get '/' do
   erb :index
 end
 
-# get "/movies/:movie_id" do
-#   db = PG.connect(dbname: 'movies', host: 'localhost')
-#   begin
-#     movie_id = params[:movie_id].to_i
-#     sql = "SELECT * FROM movies WHERE id=#{movie_id}"
-#     @movie = db.exec(sql).first #because SQL does not know that a sinle record ..... it returns it as an array...so we call it .first .. we get a hash which we can call title on in the show
-#   ensure
-#     db.close
-#   end
-#   erb :show
-# end
+get "/movies/:movie_id" do
+  db = PG.connect(dbname: 'movies', host: 'localhost')
+  begin
+    movie_id = params[:movie_id].to_i
+    sql = "SELECT * FROM movies WHERE id=#{movie_id}"
+    @movie = db.exec(sql).first #because SQL does not know that a sinle record ..... it returns it as an array...so we call it .first .. we get a hash which we can call title on in the show
+  ensure
+    db.close
+  end
+  erb :show
+end
 
-# get "/new" do
-#   @movie = {} # w initialize and empty hash inorder to initialze thie object because th if statment in erb :new requires this variable
-#   erb :new
-# end
+get "/new" do
+  @actor = {} # w initialize and empty hash inorder to initialze thie object because th if statment in erb :new requires this variable
+  erb :new
+end
 
-# post "/new" do
-#   db = PG.connect(dbname: 'movies', host: 'localhost')
-#   begin
-#     sql = "INSERT INTO movies (title, year, rated, poster, director, actors)
-#     VALUES ('#{params[:title]}', '#{params[:year]}', '#{params[:rated]}', '#{params[:poster]}', '#{params[:director]}', '#{params[:actors]}') RETURNING id"
-#     @movie = db.exec(sql)
-#     new_created_id = @movie[0]["id"]
-#     redirect "/movies/#{new_created_id}"
-#   ensure  
-#     db.close
-#   end
-#     erb :index  #use the same index so we can reuse the .each allready being used
-# end
+post "/new" do
+  db = PG.connect(dbname: 'movies', host: 'localhost')
+  begin
+    sql = "INSERT INTO actors (dob, last_name, first_name, image_url)
+    VALUES ('#{params[:dob]}', '#{params[:last_name]}', '#{params[:first_name]}', '#{params[:image_url]}' ) RETURNING id"
+    @actor = db.exec(sql)
+    new_created_id = @actor[0]["id"]
+    redirect "/actors/#{new_created_id}"
+  ensure  
+    db.close
+  end
+    erb :index  #use the same index so we can reuse the .each allready being used
+end
 
 post "/search" do
   db = PG.connect(dbname: 'movies', host: 'localhost')
