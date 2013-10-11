7e18df38aeb6942420cbd0861d721190b90e01dd
Ralph Reid <beresfordjunior@me.com>
2013-10-11 14:15:02 +0100

    classwork including refactored movie db

---
+++ ralphreid/w2d5/classwork/README.md
@@ -0,0 +1,6 @@
+
+Session: w2d4 Homework Review
+https://www.evernote.com/shard/s6/sh/3c482e8c-9092-48d3-b427-f04893bd82cb/4dc912fe9aeb525e8e6cc383765deb6c
+
+Session: Session: Refactoring App using Blocks &  Classes
+https://www.evernote.com/shard/s6/sh/4f50f42d-4be3-4be5-84b1-e11db801592a/ea7c5484cccbaf70ff001304e39b8686
--- ralphreid/w2d5/classwork/movies_app/main.rb
+++ ralphreid/w2d5/classwork/movies_app/main.rb
@@ -1,12 +1,17 @@
 require "sinatra"
 require "sinatra/contrib/all"
+require 'sinatra/reloader'
 require "pg"
 require "pry"
 
-# Configuration
+require_relative './model/movie.rb'
+also_reload './model/movie.rb'
+
+# Configuration 
+
 before do
   @db = PG.connect(dbname: "movies", host: "localhost")
-  @movie = Movie.new
+  @movie = Movie.new(@db)
 end
 
 after do
@@ -15,12 +20,12 @@ end
 
 # Routes Handlers
 get "/" do
-  @movies = @db.exec "SELECT * FROM movies"
+  @movies = @movie.all
   erb :index
 end
 
 get "/movies/:movie_id" do
-  @movie = @db.exec("SELECT * FROM movies WHERE id=#{params[:movie_id]}").first
+  @movie = @movie.find params[:movie_id]
   erb :show
 end
 
@@ -30,38 +35,27 @@ get "/new" do
 end
 
 post "/new" do
-  binding.pry
-  new_created_id = @db.exec("INSERT INTO movies (title, year, rated, poster, director, actors)
-    VALUES ('#{params[:title]}', '#{params[:year]}', '#{params[:rated]}', '#{params[:poster]}', '#{params[:director]}', '#{params[:actors]}') RETURNING id") [0]["id"]
+  new_created_id = @movie.create params #TIP this should always be called params so Sinatra can get the session hash 
   redirect "/movies/#{new_created_id}"
 end
 
 post "/search" do
-  @movies = @db.exec "SELECT * FROM movies WHERE title ILIKE '%#{params[:query]}%'"
+  @movies = @movie.search params[:query] 
   erb :index
 end
 
 get "/movies/:movie_id/update" do
-  @movie = @db.exec("SELECT * FROM movies WHERE id=#{params[:movie_id]}").first
+  @movie = @movie.find params[:movie_id]
   erb :new
 end
 
 post "/movies/:movie_id/update" do
-  movie_id = params[:movie_id]
-  sql = "UPDATE movies SET
-   title='#{params[:title]}',
-   year='#{params[:year]}',
-   rated='#{params[:rated]}', 
-   poster='#{params[:poster]}',
-   director='#{params[:director]}', 
-   actors='#{params[:actors]}' 
-   WHERE id=#{movie_id}"
-  @db.exec(sql)
-  redirect "/movies/#{movie_id}"
+  @movie.update params
+  redirect "/movies/#{params[:movie_id]}"
 end
 
 post "/movies/:movie_id/delete" do 
-  @db.exec "DELETE FROM movies WHERE id=#{params[:movie_id]}"
+  @movie.delete params[:movie_id]
   redirect "/"
 end
 
