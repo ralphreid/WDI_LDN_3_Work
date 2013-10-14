5edefda48b4204da707917c8221a413d675fd65c
Ralph Reid <beresfordjunior@me.com>
2013-10-14 09:44:51 +0100

    completed HW w2d5 - Movie App

---
+++ ralphreid/w2d5/classwork/movies_app/README.md
@@ -0,0 +1,22 @@
+Backlog
+
+DONE - Refactor main.rb 
+
+DONE - Create new foriegn key table to relating actors & movies
+
+DONE - Refactor to include models for both actos & movies
+
+DONE - Integrate Movie classes with seperated Handlers for movies & actors where appropriate
+
+
+DONE - Breaks when selecting a movie from an actor page and visa versa
+
+BUG - actual insert values into the foriegn key table
+
+BUG - Update of moves & actors breaks
+
+
+
+Enhance UI
+
+Implement referential integrity in the actors_movies table
---
+++ ralphreid/w2d5/classwork/movies_app/models/movie.rb
@@ -0,0 +1,55 @@
+class Movie
+
+  def initialize connection
+    @connection = connection
+  end
+
+  def all
+    @connection.exec "SELECT * FROM movies" #retun is implicit in ruby
+  end
+
+  def actors id
+    @connection.exec("SELECT * FROM actors
+      INNER JOIN actors_movies
+      ON actors_movies.actor_id = actors.id
+      WHERE actors_movies.movie_id= #{id}")
+  end
+
+  def find id
+    @movie = @connection.exec("SELECT * FROM movies WHERE id=#{id}").first
+  end
+
+  def create params  # TIP - called the argument params so that there was nothing to change in the method when we main
+    @connection.exec("INSERT INTO movies (title, year, rated, poster, director, actors)
+    VALUES ('#{params[:title]}', '#{params[:year]}', '#{params[:rated]}', '#{params[:poster]}', '#{params[:director]}', '#{params[:actors]}') RETURNING id") [0]["id"]
+  
+    #@connection.exec( "INSERT INTO actors_movies (movie_id, actor_id)
+      #VALUES ('#{params[:movie].to_i}', '#{params[:actor].to_i}')") 
+
+  end
+
+  def search query
+    @connection.exec "SELECT * FROM movies WHERE title ILIKE '%#{query}%'"
+  end
+
+  def find id
+    @connection.exec("SELECT * FROM movies WHERE id=#{id.to_i}").first
+  end
+
+  def update params
+    @connection.exec "UPDATE movies SET
+       title='#{params[:title]}',
+       year='#{params[:year]}',
+       rated='#{params[:rated]}', 
+       poster='#{params[:poster]}',
+       director='#{params[:director]}', 
+       actors='#{params[:actors]}' 
+       WHERE id=#{params [:movie_id]}"
+       #possible to add Insert Into here
+  end
+
+  def delete id
+    @connection.exec "DELETE FROM movies WHERE id= #{id} "
+  end
+
+end
