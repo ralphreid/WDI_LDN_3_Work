4ac7eafb09a7ff9e6406b7442c412b896e4f0238
Ralph Reid <beresfordjunior@me.com>
2013-10-16 09:00:00 +0100

    w3d2 Moon _app hw

--- ralphreid/w3d2/homework/moons_app/app/controllers/moons_controller.rb
+++ ralphreid/w3d2/homework/moons_app/app/controllers/moons_controller.rb
@@ -14,3 +14,20 @@ class MoonsController < ApplicationController
 
 
 end
+
+
+
+
+  # def create
+  #   @post = Post.new(params[:post])
+
+  #   respond_to do |format|
+  #     if @post.save
+  #       format.html { redirect_to @post, notice: 'Post was successfully created.' }
+  #       format.json { render json: @post, status: :created, location: @post }
+  #     else
+  #       format.html { render action: "new" }
+  #       format.json { render json: @post.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
