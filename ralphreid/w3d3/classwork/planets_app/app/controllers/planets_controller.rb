892824f9e3bfd6b08f87a36facbb55b6ac60c542
Ralph Reid <beresfordjunior@me.com>
2013-10-16 12:42:32 +0100

    w3d3_classwork - Planet All - end of session

--- ralphreid/w3d3/classwork/planets_app/app/controllers/planets_controller.rb
+++ ralphreid/w3d3/classwork/planets_app/app/controllers/planets_controller.rb
@@ -9,7 +9,28 @@ class PlanetsController < ApplicationController
   end
 
   def create
-    
+    planet = Planet.new params[:planet]
+    planet.save
+    redirect_to planets_url
+  end
+
+  def show
+    @planet = Planet.find params[:id]
+  end
+
+  def destroy
+    Planet.find(params[:id]).destroy
+    redirect_to planets_url
+  end
+
+  def edit
+    @planet = Planet.find params[:id]
+  end
+
+  def update
+    @planet = Planet.find params[:id]
+    @planet.update_attributes(params[:planet])
+    redirect_to @planet
   end
 
 end
