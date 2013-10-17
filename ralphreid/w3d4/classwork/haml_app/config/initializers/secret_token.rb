9a5ec2d4ee01cae1ed3d0704a7930145a6d4f700
Ralph Reid <beresfordjunior@me.com>
2013-10-17 13:36:13 +0100

    w3d4 - HAML session

---
+++ ralphreid/w3d4/classwork/haml_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HamlApp::Application.config.secret_token = '8f70af4d144e50bc21591794c87ebb1d6f4981b395d8d04baedabcd940a337cd082fba12c834a1313f222384546b0c41df0af617dc2f5e574f529330842fcc57'
