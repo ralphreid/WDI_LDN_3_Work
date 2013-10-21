9973fc133b41bd6b7f977129d2660ecaf2d27240
Ralph Reid <beresfordjunior@me.com>
2013-10-21 22:22:07 +0100

    w4d1 - initiate work on my_library_app

---
+++ ralphreid/w4d1/homework/my_library_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyLibraryApp::Application.config.secret_token = '09952ff40cc5dc959172b02b85fce92cbe792e9587a4563f1f3d4b8a00da7da75b150b2aa2b2dd5de5ef0d3d5ac8a09e8021e665226806b3171b70ae52134608'
