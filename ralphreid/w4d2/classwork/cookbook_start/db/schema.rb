6171b2b59869448790de614121880de5fd73cd84
Ralph Reid <beresfordjunior@me.com>
2013-10-22 13:53:24 +0100

    w4d2 - start of Authentication session

--- ralphreid/w4d2/classwork/cookbook_start/db/schema.rb
+++ ralphreid/w4d2/classwork/cookbook_start/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131020153220) do
+ActiveRecord::Schema.define(:version => 20131022124933) do
 
   create_table "ingredients", :force => true do |t|
     t.string   "name"
@@ -38,6 +38,14 @@ ActiveRecord::Schema.define(:version => 20131020153220) do
     t.text     "image"
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
+    t.boolean  "flagged"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.string "role"
   end
 
 end
