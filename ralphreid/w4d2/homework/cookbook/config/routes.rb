ac718808d23802b1dd5b9fd5f818a97565a9e6f3
Ralph Reid <beresfordjunior@me.com>
2013-10-22 18:19:21 +0100

    w4d2-homework start

---
+++ ralphreid/w4d2/homework/cookbook/config/routes.rb
@@ -0,0 +1,27 @@
+R20130214Cookbook::Application.routes.draw do
+  resources :ingredients
+  resources :users, only: [:index, :new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
+
+  resources :recipes do
+    member do
+      put :flag
+    end
+    collection do
+      get :flagged
+    end
+
+    resources :quantities, only: [:new, :destroy, :create]
+  end
+
+  root to:  "recipes#index"
+
+  get "/signup", to: "users#new",           as: "signup"
+  get "/login", to: "sessions#new",         as: "login"
+  delete "/logout", to: "sessions#destroy",   as: "logout"
+
+  # put "/recipes/:id/flag", to: "recipes#flag", as: :flag_recipe
+  # put "/recipes/flagged", to: "recipes#flagged", as: :flaged_recipes
+
+
+end
