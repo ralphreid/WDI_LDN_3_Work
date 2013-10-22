9592e07cd1c163eb678e9899b9894923c9b28312
Ralph Reid <beresfordjunior@me.com>
2013-10-22 16:02:56 +0100

    w4d2 - start of Authorisation session

--- ralphreid/w4d2/classwork/cookbook_start/app/controllers/users_controller.rb
+++ ralphreid/w4d2/classwork/cookbook_start/app/controllers/users_controller.rb
@@ -4,10 +4,16 @@ class UsersController < ApplicationController
   end
 
   def new
-    
+    @user = User.new
   end
 
   def create
-    
+    @user = User.new params[:user]
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_url, notice: "thank you for signing up"
+    else
+      render :new
+    end
   end
 end
--- ralphreid/w4d2/classwork/cookbook_start/config/routes.rb
+++ ralphreid/w4d2/classwork/cookbook_start/config/routes.rb
@@ -1,5 +1,7 @@
 R20130214Cookbook::Application.routes.draw do
   resources :ingredients
+  resources :users, only: [:index, :new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 
   resources :recipes do
     member do
@@ -14,9 +16,9 @@ R20130214Cookbook::Application.routes.draw do
 
   root to:  "recipes#index"
 
-  get "/signup", to: "users#new",         as: "signup"
-  get "/login", to: "sessions#new",       as: "login"
-  get "/logout", to: "session#destroy",   as: "logout"
+  get "/signup", to: "users#new",           as: "signup"
+  get "/login", to: "sessions#new",         as: "login"
+  delete "/logout", to: "sessions#destroy",   as: "logout"
 
   # put "/recipes/:id/flag", to: "recipes#flag", as: :flag_recipe
   # put "/recipes/flagged", to: "recipes#flagged", as: :flaged_recipes
