07fdaa560a51e8d76a0bdfb3c3c1443f96f0486a
Ralph Reid <beresfordjunior@me.com>
2013-10-23 11:59:00 +0100

    w4d3 - end of morning session

--- ralphreid/w4d3/classwork/file_upload/gallery_app_start/app/uploaders/image_uploader.rb
+++ ralphreid/w4d3/classwork/file_upload/gallery_app_start/app/uploaders/image_uploader.rb
@@ -3,7 +3,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
+  include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
@@ -33,9 +33,22 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :scale => [50, 50]
-  # end
+  version :thumb do
+    process resize_to_fit: [200, 200]
+  end
+
+  version :sepia_preview do
+    process :do_stuff => 2
+    process resize_to_fit: [400, 400]
+  end
+
+  def do_stuff(blur_factor)
+    manipulate! do |img|
+      img = img.sepiatone
+      img = img.auto_orient
+      img = img.radial_blur blur_factor
+    end
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
