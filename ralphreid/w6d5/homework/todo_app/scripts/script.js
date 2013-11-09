480456148f47db3784177cacc829d54430377881
Ralph Reid <beresfordjunior@me.com>
2013-11-09 12:19:02 +0000

    todo_app - working solution & start building nav buttons

--- ralphreid/w6d5/homework/todo_app/scripts/script.js
+++ ralphreid/w6d5/homework/todo_app/scripts/script.js
@@ -1,6 +1,6 @@
 $(function(){
 
-
+  // sample data
   var tasks = [
     {desc: 'sadffdsaf', stat: '1'},
     {desc: 'sdsaf', stat: '1'},
@@ -8,8 +8,12 @@ $(function(){
     {desc: 'sasdffffffdsaf', stat: '0'}
   ];
 
+// this is a test which can be removed later
   _(tasks).each(function(task){ console.log(task.desc + task.stat) });
 
+
+
+// render to page
   var application = $("#application").html();
   $('#list').html(_.template(application, {tasks: tasks}));
  
