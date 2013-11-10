cb98910ee35ca736d57420316037067c2b3f83b0
Ralph Reid <beresfordjunior@me.com>
2013-11-10 16:45:12 +0000

    todo_app - finsihed - start work on new task input

--- ralphreid/w6d5/homework/todo_app/scripts/script.js
+++ ralphreid/w6d5/homework/todo_app/scripts/script.js
@@ -2,20 +2,48 @@ $(function(){
 
   // sample data
   var tasks = [
-    {desc: 'sadffdsaf', stat: '1'},
-    {desc: 'sdsaf', stat: '1'},
-    {desc: 'sssd', stat: '0'},
-    {desc: 'sasdffffffdsaf', stat: '0'}
+    {desc: 'sadffdsaf', stat: '-1'},
+    {desc: 'sdsaf', stat: '-1'},
+    {desc: 'sssd', stat: '1'},
+    {desc: 'sasdffffffdsaf', stat: '1'}
   ];
 
-// this is a test which can be removed later
-  _(tasks).each(function(task){ console.log(task.desc + task.stat) });
 
+  renderApplication(tasks);
 
+  // onclick button actions
+  $('#all').click(function(){
+    renderApplication(tasks);
+  });
 
-// render to page
-  var application = $("#application").html();
-  $('#list').html(_.template(application, {tasks: tasks}));
+  $('#new').click(function(){
+    console.log('new button clicked')
+  });
+
+  $('#todo').click(function(){
+    todo = _(tasks).filter(function(t){return t.stat == 1})
+    renderApplication(todo);
+  });
+
+  $('#completed').click(function(){
+    completed = _(tasks).filter(function(t){return t.stat == -1})
+    renderApplication(completed);
+  });
+
+   
  
-// $
+  // fiter list based ons status flag
+
+  // render new task template
+
+  // capture new task
+
+  // add new task to the list
+
+  // render to page
+  function renderApplication(tasks){
+    var application = $("#application").html();
+    $('#list').html(_.template(application, {tasks: tasks}));
+  };
+ 
 })
