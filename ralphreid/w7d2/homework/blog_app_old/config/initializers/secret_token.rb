e9e54e008e3b215ffcef6ce4943c9afd580ff5fb
Ralph Reid <beresfordjunior@me.com>
2013-11-12 21:32:44 +0000

    comments and post released

--- ralphreid/w7d2/homework/blog_app_old/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-BlogApp::Application.config.secret_token = 'c9f09a85a4affb25dd43292e8391dae7a0bc761bbf2b8ae187d18d4223217f45fe9c91bf03d601aa4e3164b11999cddbbd270226e5a7b1eb8d96096357a96010'
