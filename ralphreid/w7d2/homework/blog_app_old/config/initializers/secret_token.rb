7ec5712ccd3e3affe095749a6d06eab45d67a033
Ralph Reid <beresfordjunior@me.com>
2013-11-12 20:54:15 +0000

    scaffold

--- ralphreid/w7d2/homework/blog_app_old/config/initializers/secret_token.rb
+++ ralphreid/w7d2/homework/blog_app_old/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-BlogApp::Application.config.secret_token = '5f715d117a937613d934919dbbdbbf3d182112e95f6efb4362c70ae8a9fc50b3f5180cd7eb555d85ef5e410b8082942e31862644edef8c5254b086e543467ad6'
+BlogApp::Application.config.secret_token = 'c9f09a85a4affb25dd43292e8391dae7a0bc761bbf2b8ae187d18d4223217f45fe9c91bf03d601aa4e3164b11999cddbbd270226e5a7b1eb8d96096357a96010'
