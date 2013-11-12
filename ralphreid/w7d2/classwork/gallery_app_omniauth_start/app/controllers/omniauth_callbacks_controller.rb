88d4743959ba8103c41302808955e892f0225a67
Ralph Reid <beresfordjunior@me.com>
2013-11-12 15:13:11 +0000

    Oauth solution

---
+++ ralphreid/w7d2/classwork/gallery_app_omniauth_start/app/controllers/omniauth_callbacks_controller.rb
@@ -0,0 +1,14 @@
+class  OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def google_oauth2
+    user = User.from_omniauth(request.env[omniauth.auth])
+    if user.persisted?
+      flash.notice = 'Signed in through Google!'
+      sign_in_and_redirect user
+    else
+      session['devise.user_attributes'] = user.attributes
+      flash.notic = 'Problem creating account'
+      redirect_to new_user_registration_url
+    end
+  end
+
+end
---
+++ ralphreid/w7d2/classwork/gallery_app_omniauth_start/app/models/user.rb
@@ -0,0 +1,38 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :lockable
+  devise  :database_authenticatable,
+          :registerable,
+          :recoverable,
+          :rememberable,
+          :trackable,
+          :validatable,
+          :confirmable,
+          :timeoutable,
+          :omniauthable,
+          :confirm_within => 10.minutes, :omniauth_providers => [:google_oauth2]
+
+  # devise param1, param2, key1 => value1, key2 => value2
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me
+  # attr_accessible :title, :body
+
+  def self.from_omniauth(auth)
+    # binding.pry #see whats in side
+      if user = User.find_by_email(auth.info.email)
+        user.provider = auth.provider
+        user.uid = auth.uid
+        user
+    else
+      where(auth.slice(:provider, :uid)).first_or_create do |user|
+        user.provider = auth.provider
+        user.uid = auth.uid
+        user.email = auth.info.email
+        user.password = Devise.friendly_token[0,20]
+        user.skip_confirmation! # dont' require email confirmation
+        user
+      end
+    end
+  end
+end
