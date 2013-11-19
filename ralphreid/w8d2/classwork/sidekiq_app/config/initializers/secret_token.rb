5acc63d84237981bac02d23584397c10fa327693
Ralph Reid <beresfordjunior@me.com>
2013-11-19 09:41:41 +0000

    sidkiq session

---
+++ ralphreid/w8d2/classwork/sidekiq_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SidekiqApp::Application.config.secret_token = '52c0691b8a28aec6a048ada42ebff48c7499d836387309db0a3dc7b64c4e58af132793dd3d8f70113b43ee1d65d197460a64380ba065a0a84103dbc483b6597c'
